43ef3f8c7c25f23b43e0971796e7af1a58256591
Anupama-Epsi <54168376+Anupama-Epsi@users.noreply.github.com>
2019-08-20 18:11:42 +0530

    added test case

---
+++ src/test/java/com/stepIn/testCases/TestCase2.java
@@ -0,0 +1,20 @@
+package com.stepIn.testCases;
+import com.stepIn.pages.*;
+
+import org.testng.annotations.Test;
+
+
+public class TestCase2 {
+	CommonMethods common = new CommonMethods();
+	
+	
+  @Test
+  public void teststep1() {
+	  System.out.println("test step1 executed");
+  }
+  
+  @Test
+  public void teststep2() {
+	  System.out.println("test step2 executed");
+  }
+}
