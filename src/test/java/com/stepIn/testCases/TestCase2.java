c7351890af16445aebcc3d4c42dd6074a21dd7b5
sumit kumar <sumit1439697@gmail.com>
2019-08-21 12:31:38 +0530

    Commting @12:31pm

--- src/test/java/com/stepIn/testCases/TestCase2.java
+++ src/test/java/com/stepIn/testCases/TestCase2.java
@@ -12,11 +12,13 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -31,12 +33,13 @@ public class TestCase2 {
 	  System.out.println("test step1 executed");
 	  
 	  //Once video will be scroll down to center the page take screenshot 
+	  System.setProperty("webdriver.chrome.driver", "Utils/Drivers/chromedriver.exe");
 	  WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com/");
 		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		
 		try {
-			FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
+			FileUtils.copyFile(scrFile, new File("testNgMavenExample\\screenshots\\screenshot.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -48,10 +51,28 @@ public class TestCase2 {
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath))).click();
 		//driver.findElement(By.xpath("//a[contains(text(),'DSTC 2019 | Khimananad Upreti')]")).click();
 		
+		driver.findElement(By.cssSelector("button.ytp-button.ytp-settings-button")).click();
+		// Actions actions =new Actions(driver);
+		 WebElement element=driver.findElement(By.cssSelector("button.ytp-button.ytp-settings-button"));
+		 element.sendKeys(Keys.ARROW_DOWN);
+		 element.sendKeys(Keys.ARROW_DOWN);
+		 element.sendKeys(Keys.ENTER);
+		 element.sendKeys(Keys.ARROW_UP);
+		 element.sendKeys(Keys.ARROW_UP);
+		 element.sendKeys(Keys.ARROW_UP);
+		 element.sendKeys(Keys.ENTER);
+		 
+		 
 		
+		List <WebElement> upNextVideosList= driver.findElements(By.cssSelector("div.style-scope ytd-watch-next-secondary-results-renderer ytd-compact-autoplay-renderer.style-scope.ytd-watch-next-secondary-results-renderer"));
+		   List<String> upNextYoutubevideoList =new ArrayList<String>();
+			for(int i=0;upNextVideosList.size()>i;i++) {
+				String videoList=upNextVideosList.get(i).getText().trim();
+				System.out.println(videoList);
+				upNextYoutubevideoList.add(videoList);
+			}
 		
-		List <WebElement> upNextVideosList= new ArrayList();
-		upNextVideosList.
+		//div.style-scope ytd-watch-next-secondary-results-renderer
   }
   
   @Test
